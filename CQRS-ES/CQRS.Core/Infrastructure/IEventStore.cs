6244bff2edb5fcaad33132b2fd2ddca0487f9cab
Huy Nguyễn <huyntp1103@gmail.com>
2023-10-17 23:18:06 +0700

    Implement event store, config write database

---
+++ CQRS-ES/CQRS.Core/Infrastructure/IEventStore.cs
@@ -0,0 +1,12 @@
+using System.Runtime.CompilerServices;
+using CQRS.Core.Events;
+
+namespace CQRS.Core.Infrastructure
+{
+    public interface IEventStore
+    {
+        Task SaveEventsAsync(Guid aggregateId, IEnumerable<BaseEvent> events, int expectedVersion);
+
+        Task<List<BaseEvent>> GetEventsAsync(Guid aggregateId);
+    }
+}
